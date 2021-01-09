13b9bf8914e59fdacec04c8b545f657b16658425
Victor C. Rozario <victorroz@hotmail.com>
2021-01-09 20:48:08 +0600

    Fixed Code

--- Enormous Input Test/main.cpp
+++ Enormous Input Test/main.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
-#include <vector>
-
-int enormous(int n, int k)
-{
-    int count = 0;
-    std::vector<int> t;
-
-    for(int i = 0; i < n; i++)
-    {
-        int num;
-        std::cin >> num;
-        t.push_back(num);
-    }
-
-    for(int i = 0; i < n; i++)
-    {
-        if(t[i] % k == 0) count++;
-    }
-
-    return count;
-}
+using namespace std;
 
 int main()
 {
-    int n, k;
+    int n, k, count = 0, num;
 
-    std::cin >> n;
-    std::cin >> k;
+    cin >> n >> k;
 
-    std::cout << enormous(n, k);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> num;
+
+        if(num % k == 0) count++;
+    }
+
+    cout << count;
 
     return 0;
 }
